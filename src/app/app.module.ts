619ee244bb016426fbd4936ca0ae598c1c176001
Gabriel Race <gabriel-race@outlook.com>
2018-01-30 09:53:05 -0300

    Aplicativo completo (Native Geocoder)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { NativeGeocoder } from '@ionic-native/native-geocoder';
+
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { GeocoderProvider } from '../providers/geocoder/geocoder';
 
 @NgModule({
   declarations: [
@@ -14,6 +18,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -24,7 +29,9 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    NativeGeocoder,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    GeocoderProvider
   ]
 })
 export class AppModule {}
